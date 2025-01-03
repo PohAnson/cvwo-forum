d517b021b19731c7a0effbc79f45a5b069fbe567
Poh Anson <50758680+PohAnson@users.noreply.github.com>
2025-01-03 15:49:35 +0800

    Backend Signup Api

---
+++ cvwo-forum-backend/internal/database/database.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+var dbInstance *sql.DB
+var once sync.Once
+
+func GetDb() *sql.DB {
+	if dbInstance == nil {
+		once.Do(func() {
+			connStr := os.Getenv("POSTGRESDB_URL")
+			db, err := sql.Open("postgres", connStr)
+			if err != nil {
+				log.Fatalln(err)
+			} else {
+				dbInstance = db
+			}
+		})
+	}
+	return dbInstance
+}
+func MakeMigration(dbName string) {
+	if dbInstance == nil {
+		GetDb()
+	}
+	driver, err := postgres.WithInstance(dbInstance, &postgres.Config{})
+	if err != nil {
+		log.Fatalln("Error getting postgres instance: ", err)
+	}
+	m, err := migrate.NewWithDatabaseInstance("file://internal/database/migration", dbName, driver)
+	if err != nil {
+		log.Fatalln("Creating new migration instance error: ", err)
+	}
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatalln("Error applying migration: ", err)
+	}
+}
